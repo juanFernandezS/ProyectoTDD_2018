1182429119958c8f14b810b7e8b270fbd5b5fd63
Juan Fernandez <juan.e.fernandez.suazo@gmail.com>
2018-06-06 15:46:49 -0400

    Primera subida proyecto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="media">
+                <div class="media-left">
+                    <a href="#">
+                        <img class="media-object" src="" alt="...">
+                    </a>
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">Media heading</h4>
+                    ESTE SERIA EL CUERPO
+                </div>
+            </div>
+
+            <div class="media">
+                <div class="media-left">
+                    <a href="#">
+                        <img class="media-object" src="" alt="...">
+                    </a>
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">Media heading</h4>
+                    ESTE SERIA EL CUERPO
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+    <div id="mi-carousel" class="carousel slide">
+
+        <!-- Contenedor de los Slide -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="" class="img-responsive" alt="" >
+            </div>
+                <div class="item">
+                    <img src="" alt="" class="img-responsive">
+                </div>
+        </div>
+
+        <!-- Controloles -->
+        <a class="left carousel-control" href="#mi-carousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+        </a>
+        <a class="right carousel-control" href="#mi-carousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Siguiente hidden-xs hidden-sd</span>
+        </a>
+    </div>
+@endsection
