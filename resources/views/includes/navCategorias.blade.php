5c2bea47613acd90c7345616908c9fac5820da30
Felipe Rosas <felipe.rosas1501@alumnos.ubiobio.cl>
2018-07-03 13:53:41 -0400

    vistas y arreglo de plantilla

--- resources/views/includes/navCategorias.blade.php
+++ resources/views/includes/navCategorias.blade.php
@@ -1,7 +1,8 @@
 <div>
-    <h5>Categorias</h5>
+
     <nav class="navbar navbar-light">
         <ul class="list-group">
+            <li class="list-group-item list-group-item-action active" href="#">Categorias</li>
             @foreach($categorias as $categoria)
                 <li class="list-group-item">
                     <a class="nav-link" href="#">{{ $categoria->nombre }} </a>
